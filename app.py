a5f92bd8e2a2d32bc8bf2a17fb2afeeea74617d0
vlahovicnikola <vlahovic@me.com>
2025-06-18 11:08:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, send_file, request
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
@@ -17,18 +16,9 @@ def timer():
 
     now = datetime.utcnow()
     delta = end_time - now
-    if delta.total_seconds() <= 0:
-        values = ["00", "00", "00", "00"]
-    else:
-        days = delta.days
-        hours, rem = divmod(delta.seconds, 3600)
-        minutes, seconds = divmod(rem, 60)
-        values = [f"{days:02}", f"{hours:02}", f"{minutes:02}", f"{seconds:02}"]
 
-    labels = ["Dana", "Sati", "Minuta", "Sekundi"]
-
-    # Slika i fontovi
-    img = Image.new("RGB", (450, 150), color=(255, 255, 255))
+    # Transparentna slika
+    img = Image.new("RGBA", (450, 150), color=(255, 255, 255, 0))
     draw = ImageDraw.Draw(img)
 
     try:
@@ -38,21 +28,31 @@ def timer():
         font_main = ImageFont.load_default()
         font_label = ImageFont.load_default()
 
-    # Razmakni svaki segment (4 ukupno)
-    segment_width = 450 // 4
+    if delta.total_seconds() <= 0:
+        message = "PONUDA JE ISTEKLA!"
+        w = draw.textlength(message, font=font_main)
+        draw.text(((450 - w) // 2, 50), message, font=font_main, fill=(255, 0, 0, 255))
+    else:
+        days = delta.days
+        hours, rem = divmod(delta.seconds, 3600)
+        minutes, seconds = divmod(rem, 60)
 
-    for i in range(4):
-        value = values[i]
-        label = labels[i]
+        values = [f"{days:02}", f"{hours:02}", f"{minutes:02}", f"{seconds:02}"]
+        labels = ["Dana", "Sati", "Minuta", "Sekundi"]
+        segment_width = 450 // 4
 
-        v_bbox = font_main.getbbox(value)
-        v_width = v_bbox[2] - v_bbox[0]
-        v_x = segment_width * i + (segment_width - v_width) // 2
-        draw.text((v_x, 30), value, font=font_main, fill=(255, 0, 0))
+        for i in range(4):
+            value = values[i]
+            label = labels[i]
 
-        l_width = draw.textlength(label, font=font_label)
-        l_x = segment_width * i + (segment_width - l_width) // 2
-        draw.text((l_x, 95), label, font=font_label, fill=(0, 0, 0))
+            v_bbox = font_main.getbbox(value)
+            v_width = v_bbox[2] - v_bbox[0]
+            v_x = segment_width * i + (segment_width - v_width) // 2
+            draw.text((v_x, 30), value, font=font_main, fill=(255, 0, 0, 255))
+
+            l_width = draw.textlength(label, font=font_label)
+            l_x = segment_width * i + (segment_width - l_width) // 2
+            draw.text((l_x, 95), label, font=font_label, fill=(0, 0, 0, 255))
 
     buf = BytesIO()
     img.save(buf, format='PNG')
